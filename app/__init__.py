07e2e30345f79b80ff20a31f3a0ecf787214e30c
Luciano Filho <lvgalvaofilho@gmail.com>
2023-06-19 15:34:51 -0300

    remove config.py

--- app/__init__.py
+++ app/__init__.py
@@ -34,8 +34,8 @@ JOBS = [
 
 app = Flask(__name__)
 
-# Configurações do aplicativo
-app.config.from_pyfile('config.py')
+# # Configurações do aplicativo
+# app.config.from_pyfile('config.py')
 
 # Importando os módulos/routes
 from app import models, routes
